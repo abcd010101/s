d1c82c6ef3b13367fd8c89c67929bdfef65a6b61
eee <73144446+abcd010101@users.noreply.github.com>
2026-05-29 15:02:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([43.7166; -79.7888]).addTo(map);
+
+marker.bindPopup("very <b>cool</b> place. <br> hlss coords").openPopup();

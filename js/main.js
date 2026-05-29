5e387967e80bcd228bb91db73ea4fa66a8a5d470
eee <73144446+abcd010101@users.noreply.github.com>
2026-05-29 14:55:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([43.7155517, -79.7245568], 13);
+var map = L.map('map').setView([43.6849, -79.7593], 20);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,

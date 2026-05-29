5fe2434d4d9c2c0bbe5862228629dc6eda93575c
eee <73144446+abcd010101@users.noreply.github.com>
2026-05-29 14:35:03 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([43.7155517, -79.7245568], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

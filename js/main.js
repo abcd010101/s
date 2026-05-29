16076f8155195f9f9c4c61bb549dc744fefa85f9
eee <73144446+abcd010101@users.noreply.github.com>
2026-05-29 15:06:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([43.7166; -79.7888], 15);
+var map = L.map('map').setView([43.7166, -79.7888], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([43.7166; -79.7888]).addTo(map);
+var marker = L.marker([43.7166, -79.7888]).addTo(map);
 
 marker.bindPopup("very <b>cool</b> place. <br> hlss coords").openPopup();
